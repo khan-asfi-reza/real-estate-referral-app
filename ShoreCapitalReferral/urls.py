94fa13fcb3da27be4033b21fbc0080c847c54ead
Khan Asfi Reza <khanasfireza10@gmail.com>
2021-07-26 00:40:25 +0600

    Production Batch Fix

--- ShoreCapitalReferral/urls.py
+++ ShoreCapitalReferral/urls.py
@@ -4,8 +4,8 @@ from django.urls import path, include
 from . import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("api/core/", include("Core.urls")),
-    path("api/auth/", include("Account.urls"))
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
- + static(settings.MEDIA_URL,  document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path("api/core/", include("Core.urls")),
+                  path("api/auth/", include("Account.urls"))
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+              + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
