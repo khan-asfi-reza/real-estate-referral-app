6a712ab81f4f206dd0566c9a7d511b737b8d0f80
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-05-07 00:37:45 +0600

    Test cases

--- Core/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-from Core.tests.integration_test import IntegrationTest
-from Core.tests.recruiter_test import RecruiterTest
-from Core.tests.recruit_test import RecruitTest
-from Core.tests.referral_test import ReferralTest
-from Core.tests.transaction_test import TransactionTest
